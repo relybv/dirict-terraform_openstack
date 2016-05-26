5bd8041d704613c8f749cccaa0f038ddd1340028
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-26 15:31:48 +0000

    reorder output order

--- outputs.tf
+++ outputs.tf
@@ -2,15 +2,15 @@ output "customer" {
     value = "${var.customer}"
 }
 
-output "environment" {
+output "customer environment" {
     value = "${var.environment}"
 }
 
-output "fqdn" {
+output "customer fqdn" {
     value = "${concat(var.ext_lb_name,".",var.environment,".",var.customer,".",var.domain_base)}"
 }
 
-output "nameservers" {
+output "customer nameservers" {
     value = "${aws_route53_zone.environment.name_servers.0}, ${aws_route53_zone.environment.name_servers.1}, ${aws_route53_zone.environment.name_servers.2}, ${aws_route53_zone.environment.name_servers.3}"
 }
 
@@ -22,7 +22,7 @@ output "load balancer public address" {
     value = "${openstack_compute_floatingip_v2.float1.address}"
 }
 
-output "test https servers" {
+output "application servers https working test" {
     value = "https://${openstack_compute_floatingip_v2.float1.address}/working.html"
 }
 
@@ -42,7 +42,7 @@ output "monitor server 1 address" {
     value = "${openstack_compute_floatingip_v2.float5.address}"
 }
 
-output "jump server login" {
+output "monitor server login" {
     value = "ssh ubuntu@${openstack_compute_floatingip_v2.float5.address} -i ~/.ssh/id_rsa.terraform"
 }
 
@@ -50,7 +50,7 @@ output "windows server 1 address" {
     value = "${openstack_compute_floatingip_v2.float6.address}"
 }
 
-output "get windows server 1 password" {
+output "windows server get password" {
     value = "nova --insecure get-password win1 ~/.ssh/id_rsa.terraform"
 }
 
